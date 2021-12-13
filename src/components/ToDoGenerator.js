bce3da44209049f32a5e7f806870ffdd8543e6e8
ANNA TSANG <TSANGAN@OOCL.COM>
2021-12-13 18:50:01 +0800

    [react-todolist][Anna] feat: input text can be recorded

--- src/components/ToDoGenerator.js
+++ src/components/ToDoGenerator.js
@@ -1,10 +1,24 @@
+import { useState } from "react";
+
 function ToDoGenerator(){
 
+    const [todoText, setTodoText] = useState("");
+
+    function handleToDoText(event) {
+        setTodoText(event.target.value);
+    }
+
+    function submitToDoText(event) {
+        event.preventDefault();
+        console.log(todoText);
+    }
+    
+
     return(
-        <div>
-            <input type="text"></input>
+        <form onSubmit={submitToDoText}>
+            <input type="text" onChange={handleToDoText}></input>
             <input type="submit"></input>
-        </div>
+        </form>
     );
 }
 
