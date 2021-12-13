eb93fd5619db2832468443811c372be039ee5670
ANNA TSANG <TSANGAN@OOCL.COM>
2021-12-13 18:27:47 +0800

    [react-todolist][Anna] feat: toDoList render

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,7 +1,12 @@
+import { useState } from "react";
+
 function ToDoList() {
+
+    const [todoText, setTodoText] = useState("This is the first todo item.");
+    
     return (
         <div>
-
+            <p>{todoText}</p>
         </div>
     );
 }
