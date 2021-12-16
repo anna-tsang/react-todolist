71aa7323f6ba0d339ad7bc008a4c6a38057b6dca
ANNA TSANG <TSANGAN@OOCL.COM>
2021-12-16 18:10:27 +0800

    [react-antD][Anna] feat: connect to BE

--- src/apis/api.js
+++ src/apis/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://61b9891938f69a0017ce60d7.mockapi.io/'
+    baseURL: 'http://localhost:8080'
 });
 
 export default api;
